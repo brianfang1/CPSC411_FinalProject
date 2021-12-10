ed5a1d640fded6681d0d3acf2f143a1665e51921
awesomeaj5 <43348940+awesomeaj5@users.noreply.github.com>
2021-12-10 14:21:36 -0800

    Created DropDown

--- NBATransactionApp/NBATransactionApp/AddScreen.swift
+++ NBATransactionApp/NBATransactionApp/AddScreen.swift
@@ -2,17 +2,27 @@
 //  AddScreen.swift
 //  NBATransactionApp
 //
-//  Created by csuftitan on 12/9/21.
+//  Created by csuftitan on 12/10/21.
 //
 
 import UIKit
 
-class AddScreen: UIViewController {
 
+class AddScreen: UIViewController {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var store = DataStore.get()
+        
+        // Loads database with team info and player info
+        // ** ONLY RUN ONCE WHEN INITIALIZING DATABASE **
+         //insertTeams(store: store)
+         //insertPlayers(store: store)
     }
-
-
+    
+    
+    
 }
+
+
