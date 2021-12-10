86a5a459ca3426dbdb316494f44392bc7ce86966
awesomeaj5 <43348940+awesomeaj5@users.noreply.github.com>
2021-12-09 19:46:48 -0800

    Add label

---
+++ NBATransactionApp/NBATransactionApp/AddScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AddScreen.swift
+//  NBATransactionApp
+//
+//  Created by csuftitan on 12/9/21.
+//
+
+import UIKit
+
+class AddScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
